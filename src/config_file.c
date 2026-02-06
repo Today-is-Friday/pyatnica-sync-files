76285036de59fcf04f4e412a3bfb2f21145df16b
livardbr <livardbr@student.21-school.ru>
2026-02-06 22:08:16 +0300

    fix: makes a new line after added new path

--- src/config_file.c
+++ src/config_file.c
@@ -11,11 +11,12 @@
 
 #include "file_handle.h"
 
-int add_to_config(char const *config_path, char const *dir_path) {
+int add_to_config(char const* config_path, char const* dir_path) {
   int state = 1;
-  FILE *config_file = fopen(config_path, "a");
+  FILE* config_file = fopen(config_path, "a");
   if (config_file != NULL) {
     fputs(dir_path, config_file);
+    fputc('\n', config_file);
     fclose(config_file);
   } else
     state = 0;
@@ -43,7 +44,7 @@ int add_to_config(char const *config_path, char const *dir_path) {
 // 1 - file exists.
 // 0 - dir / file doesn't exist.
 // -1 - user has issue (no auto fix).
-int config_file_exist(char const *file_name) {
+int config_file_exist(char const* file_name) {
   int state = 1;
   // Gets path length
   int path_length = get_path_length(file_name);
@@ -59,7 +60,7 @@ int config_file_exist(char const *file_name) {
 }
 
 // Calls if config_file_exist() returned 0
-int fix_config_file(char const *file_name) {
+int fix_config_file(char const* file_name) {
   int state = 1;
   // Gets path length
   int path_length = get_path_length(file_name);
@@ -72,7 +73,7 @@ int fix_config_file(char const *file_name) {
 
   // Tries recreate a file
   if ((state = get_file_path(path, file_name))) {
-    FILE *file = fopen(path, "a");
+    FILE* file = fopen(path, "a");
     if (file != NULL)
       fclose(file);
     else
@@ -81,7 +82,7 @@ int fix_config_file(char const *file_name) {
   return state;
 }
 
-int get_path_length(char const *file_name) {
+int get_path_length(char const* file_name) {
   int path_length = 0;
   // gets max user name length
   long user_length = sysconf(_SC_LOGIN_NAME_MAX);
@@ -97,7 +98,7 @@ int get_path_length(char const *file_name) {
 
 int get_dir_path(char dir_path[]) {
   int state = 1;
-  const struct passwd *pwd = getpwuid(getuid());
+  const struct passwd* pwd = getpwuid(getuid());
 
   // Can find the user
   if (pwd) {
@@ -109,7 +110,7 @@ int get_dir_path(char dir_path[]) {
   return state;
 }
 
-int get_file_path(char file_path[], char const *file_name) {
+int get_file_path(char file_path[], char const* file_name) {
   // Gets dir path firs
   int state = get_dir_path(file_path);
 
